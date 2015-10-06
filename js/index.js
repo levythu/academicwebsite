81bd1e8ed785a5165eef48e3ad2ea51806503e79
Levy <zly.george@163.com>
2015-10-06 16:15:05 +0800

    update <a>

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ $(document).ready(function()
 {
     $(".LvLinkedToIMG").each(function(id, dom)
     {
-        dom.innerHTML="<a href=\""+$(dom).parent().children("img").attr("src")+"\">"+dom.innerHTML;
+        dom.innerHTML="<a href=\""+$(dom).parent().children("img").attr("src")+"\">"+dom.innerHTML+"</a>";
+    });
+    $(".LvLinkedToINNERIMG").each(function(id, dom)
+    {
+        dom.innerHTML="<a href=\""+$(dom).children("img").attr("src")+"\">"+dom.innerHTML+"</a>";
     });
 });
